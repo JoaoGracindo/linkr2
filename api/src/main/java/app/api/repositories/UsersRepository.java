74e3a1d2998c1c61ab93258e8598cc00e166176a
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-09-29 17:28:56 -0300

    Feat: find user by email repo method

--- api/src/main/java/app/api/repositories/UsersRepository.java
+++ api/src/main/java/app/api/repositories/UsersRepository.java
@@ -2,7 +2,8 @@ package app.api.repositories;
 
 import app.api.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+    UserDetails findByEmail(String email);
 }
