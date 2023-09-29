dc6c381bfa3279c5959d40ca2a86c369df2e7a59
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-09-29 17:28:18 -0300

    Feat: Auth service

---
+++ api/src/main/java/app/api/services/AuthorizationService.java
@@ -0,0 +1,22 @@
+package app.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import app.api.repositories.UsersRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usersRepository.findByEmail(username);
+        }
+    
+}
