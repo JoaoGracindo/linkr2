5671bbd4d44a216e5baca89646920e46d592cbd8
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-09-29 17:46:59 -0300

    Feat: login controller

---
+++ api/src/main/java/app/api/controllers/AuthenticationController.java
@@ -0,0 +1,30 @@
+package app.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.api.DTOs.AuthenticationDTO;
+import app.api.repositories.UsersRepository;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@Validated @RequestBody AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+}
