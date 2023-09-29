ec6b10ef790fc3eae6eb605e17b17b15f0ada0e3
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-09-29 18:20:55 -0300

    Feat: signup route

--- api/src/main/java/app/api/controllers/AuthenticationController.java
+++ api/src/main/java/app/api/controllers/AuthenticationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.api.DTOs.AuthenticationDTO;
+import app.api.DTOs.SignupDTO;
+import app.api.models.Users;
 import app.api.repositories.UsersRepository;
 
 @RestController
@@ -20,10 +23,24 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UsersRepository usersRepository;
+
     @PostMapping("/login")
     public ResponseEntity login(@Validated @RequestBody AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@Validated @RequestBody SignupDTO data){
+        if(this.usersRepository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        Users user = new Users(data.name(), encryptedPassword, data.email());
+        this.usersRepository.save(user);
 
         return ResponseEntity.ok().build();
     }
