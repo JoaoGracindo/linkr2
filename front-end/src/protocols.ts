b38c83cf61c28d545afb578bfe74baba44adbefb
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-10-03 20:03:56 -0300

    Feat: sign-up DTO

--- front-end/src/protocols.ts
+++ front-end/src/protocols.ts
@@ -2,3 +2,10 @@ export type loginDTO = {
     email: string,
     password: string
 }
+
+export type signUpDTO = {
+    email: string,
+    password: string,
+    picUrl: string,
+    name: string
+}
