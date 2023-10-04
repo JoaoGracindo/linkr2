c1ea7bfac4917eca6f67758b2c66837c9b16a08a
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-10-03 22:12:11 -0300

    Feat: register component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
