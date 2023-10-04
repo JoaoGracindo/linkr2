d801559626a205c1f9e601e91f26eb1ba3977c9b
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-10-04 08:57:36 -0300

    Feat: register adaptation

--- front-end/src/app/components/register/register.component.ts
+++ front-end/src/app/components/register/register.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    username: new FormControl(''),
+    picUrl: new FormControl(''),
+  });
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
