2798d74574609a43eb12b26eb8a4612f706c8034
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-10-04 08:57:20 -0300

    Fix: login route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'signup', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
 ];
 
 @NgModule({
