27d077a9af0e2c78829cd4064adf4bf1fc98393a
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-10-10 23:00:46 -0300

    Refac: localstorage inside login component

--- front-end/src/app/services/auth.service.ts
+++ front-end/src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs'; 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { loginDTO, signUpDTO } from 'src/protocols';
