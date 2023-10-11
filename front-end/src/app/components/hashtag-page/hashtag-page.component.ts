ed7aa6f1f5f54d916d385d81698192cacccb9bfe
JoaoGracindo <gabrielpessoa97@gmail.com>
2023-10-11 16:21:19 -0300

    Feat: hashtag page

---
+++ front-end/src/app/components/hashtag-page/hashtag-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hashtag-page',
+  templateUrl: './hashtag-page.component.html',
+  styleUrls: ['./hashtag-page.component.scss']
+})
+export class HashtagPageComponent {
+
+}
