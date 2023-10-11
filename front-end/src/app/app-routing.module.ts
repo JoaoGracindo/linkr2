e7744eb632bce07e896fe58e3aa1d73c29e11d2d
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-10-10 23:41:08 -0300

    Feat: feed component auth guard

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { authGuard } from './shared/auth.guard';
 
 const routes: Routes = [
-  { path: 'signup', component: RegisterComponent },
   { path: '', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  {
+    path: 'feed',
+    component: FeedComponent,
+    canActivate: [authGuard]
+  }
 ];
 
 @NgModule({
